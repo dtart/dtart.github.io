ed7cc20a50a6b923111b232881bcbbcc4b6c7953
Tomasz Sikora <frakti7@gmail.com>
2016-06-26 14:33:52 +0200

    migrate to github pages

---
+++ scripts.js
@@ -0,0 +1,25 @@
+﻿$().ready(function()
+{
+	$("#projects").mouseover(function(){
+		$("#desc").hide();
+		$("#desc").text("projektowanie instalacji elektrycznych");
+		$("#desc").fadeIn();
+    }).mouseout(function(){
+		$("#desc").fadeOut();
+    });
+
+	$("#foto").mouseover(function(){
+		$("#desc").hide();
+		$("#desc").text("fotografia").fadeIn();
+    }).mouseout(function(){
+		$("#desc").fadeOut();
+    });
+
+	$("#video").mouseover(function(){
+		$("#desc").hide();
+		$("#desc").text("wideofilmowanie").fadeIn();
+    }).mouseout(function(){
+		$("#desc").fadeOut();
+    });
+
+});
